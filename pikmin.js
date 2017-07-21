02021f161cd70ca31912c08b529e16af089a9b9a
colorlessenergy <brianmunozdev@gmail.com>
2017-07-20 17:14:34 -0700

    desktop design finished

--- pikmin.js
+++ pikmin.js
@@ -1,32 +1,32 @@
 const Pikmins = {
   "red": {
-    title: "red Pikmin",
-    text: "the red Pikmin is one of the first pikmin to be released and is resistance to fire",
+    title: "Red Pikmin",
+    text: "the red Pikmin is one of the first pikmin to be released and has resistance to fire",
     img: "assets/redpikmin.png",
     link: "http://pikmin.wikia.com/wiki/Red_Pikmin"
   },
   "blue": {
-    title: "blue Pikmin",
-    text: "the blue Pikmin is one of the first Pikmin to be released and is resistance to water",
+    title: "Blue Pikmin",
+    text: "the blue Pikmin is one of the first Pikmin to be released and has resistance to water",
     img: "assets/bluepikmin.png",
     link: "http://pikmin.wikia.com/wiki/Blue_Pikmin?file=BluePikminHD.png"
   },
   "yellow": {
-    title: "yellow Pikmin",
+    title: "Yellow Pikmin",
     text: "the yellow Pikmin is one of the first Pikmin to be released and has resistance to electricity",
     img: "assets/yellowpikmin.png",
     link: "http://pikmin.wikia.com/wiki/Yellow_Pikmin"
   },
   "purple": {
-    title: "purple Pikmin",
-    text: "the purple pikmin is one of the first Pikmin to be released and is the strongest one of the bunch",
+    title: "Purple Pikmin",
+    text: "the purple pikmin is one of the first Pikmin to be released and is the strongest one of all pikmins",
     img: "assets/purplepikmin.png",
     link: "http://pikmin.wikia.com/wiki/Purple_Pikmin"
   },
   "white": {
-    title: "white Pikmin",
+    title: "White Pikmin",
     text: "the white pikmin is one of the first Pikmin to be released and has resistance against poison",
-    img: "assets/yellowpikmin.png",
+    img: "assets/whitepikmin.png",
     link: "http://pikmin.wikia.com/wiki/Yellow_Pikmin"
   }
 }
