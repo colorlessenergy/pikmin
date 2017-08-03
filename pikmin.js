a88c0ed219f7e824ea07e1f3d4406f0ac6c184c9
colorlessenergy <brianmunozdev@gmail.com>
2017-08-03 21:26:50 +0000

    Updated 'pikmin.js' via CloudCannon

--- pikmin.js
+++ pikmin.js
@@ -2,19 +2,19 @@ const Pikmins = {
   "red": {
     title: "Red Pikmin",
     text: "the red Pikmin is one of the first pikmin to be released and has resistance to fire",
-    img: "{{site.baseurl}}/assets/redpikmin.png",
+    img: "pikmin/assets/redpikmin.png",
     link: "http://pikmin.wikia.com/wiki/Red_Pikmin"
   },
   "blue": {
     title: "Blue Pikmin",
     text: "the blue Pikmin is one of the first Pikmin to be released and has resistance to water",
-    img: "{{site.baseurl}}/assets/bluepikmin.png",
+    img: "pikmin/assets/bluepikmin.png",
     link: "http://pikmin.wikia.com/wiki/Blue_Pikmin?file=BluePikminHD.png"
   },
   "yellow": {
     title: "Yellow Pikmin",
     text: "the yellow Pikmin is one of the first Pikmin to be released and has resistance to electricity",
-    img: "{{site.baseurl}}/assets/yellowpikmin.png",
+    img: "pikmin/assets/yellowpikmin.png",
     link: "http://pikmin.wikia.com/wiki/Yellow_Pikmin"
   },
   "purple": {
@@ -26,7 +26,7 @@ const Pikmins = {
   "white": {
     title: "White Pikmin",
     text: "the white pikmin is one of the first Pikmin to be released and has resistance against poison",
-    img: "{{site.baseurl}}/assets/whitepikmin.png",
+    img: "/assets/whitepikmin.png",
     link: "http://pikmin.wikia.com/wiki/Yellow_Pikmin"
   }
 }
